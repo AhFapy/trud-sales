c3a9dd1b5d6bdebf58b54466d9dfc9b03ccb6d81
Ahmed Mirza <ahmed@Ahmeds-MacBook-Air.local>
2025-07-26 18:20:18 +0800

    a

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,14 +33,33 @@ const Contact = () => {
 
 
   return (
-    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex-gap-10 overflow-hidden">
-        <p className="text-2xl font-bold text-center">
-          ¿Quieres empezar a aumentar tus ventas?
-        </p>
-        <p className="text-2xl font-bold color-verde text-center mt-4">
-          <a href="https://app.trudsales.com/trabaja-con-nosotros">Pulsa Aquí</a>
-        </p>
-    </div>
+<div className="text-center mt-16">
+  <h3
+    className="text-[2rem] md:text-5xl font-semibold mb-12"
+    style={{ color: '#00FF00' }}
+  >
+    ¿Qué eliges?
+  </h3>
+
+  <div className="flex flex-col md:flex-row items-center justify-center gap-4">
+    <a
+      href="https://app.trudsales.com/trabaja-con-nosotros"
+      className="bg-black text-center text-[#00FF00] w-full md:w-auto font-semibold py-3 px-6 rounded-lg border-2 transition-all duration-300 hover:text-black hover:bg-[#00FF00]"
+      style={{ borderColor: '#00FF00' }}
+    >
+      Empezar a trabajar con TRUD
+    </a>
+
+    <a
+      href="https://app.trudsales.com/formate-con-nosotros"
+      className="bg-black text-center text-[#00FF00] w-full md:w-auto font-semibold py-3 px-6 rounded-lg border-2 transition-all duration-300 hover:text-black hover:bg-[#00FF00]"
+      style={{borderColor: '#00FF00' }}
+    >
+      Formarme con TRUD
+    </a>
+  </div>
+</div>
+
   )
 }
 
